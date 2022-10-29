58fb447b14d377cf221db0bed22c9412b2a85e14
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-10-28 23:02:04 -0400

    Time: 3 ms (7.61%), Space: 43.2 MB (63.85%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+        int value = 0;
+        int data = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                value = i;
+                break;
+            } else if (nums[i] < target) {
+                data = i + 1;
+            }
+        }
+        if (value != 0) {
+            return value;
+        } else {
+            return data;
+        }
+    }
+}
