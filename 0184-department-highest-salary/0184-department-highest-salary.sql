d63f142e6d24396ee5ccf10c15a91f8496272419
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-11-15 12:18:35 +0530

    Time: 1119 ms (27.86%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
-SELECT    D.name AS DEPARTMENT , 
-          E.name AS EMPLOYEE , 
-		  E.salary
-     FROM EMPLOYEE AS E 
-	               JOIN 
-				   DEPARTMENT AS D 
-				   ON E.departmentId = D.id
-     WHERE E.salary =   ( SELECT MAX(salary) # this subquery gives max salary  of an department so that we can generate all employees who have max salary in that department.
+SELECT    d.name AS DEPARTMENT , 
+          e.name AS EMPLOYEE , 
+		  e.salary
+     FROM EMPLOYEE e, 
+				   DEPARTMENT d
+				   where e.departmentId = d.id
+     and e.salary = ( SELECT MAX(salary) 
                                  FROM EMPLOYEE
-                                 WHERE departmentID = E.departmentID);
+                                 WHERE departmentID = e.departmentID);
