bc940bc0dd9422d78a7a7692527a3dcae724a3c0
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-11-19 12:40:36 +0530

    Time: 13 ms (74.10%), Space: 42.7 MB (71.73%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i))) {
+                if (map.get(t.charAt(i)) == s.charAt(i)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else if (map.containsValue(s.charAt(i))) {
+                return false;
+            } else {
+                map.put(t.charAt(i), s.charAt(i));
+            }
+        }
+        return true;
+    }
+}
