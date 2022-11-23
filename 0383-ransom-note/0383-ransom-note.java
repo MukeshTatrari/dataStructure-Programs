9dbe51085a8de9bc4eab5e27dd9d08bb06ca9714
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-11-23 11:36:44 +0530

    Time: 8 ms (67.55%), Space: 46 MB (65.04%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,19 @@
 class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+
         for (char c : magazine.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            alphabets_counter[c - 'a']++;
         }
 
         for (char c : ransomNote.toCharArray()) {
-            if (!map.containsKey(c) || map.get(c) < 1) {
+            if (alphabets_counter[c - 'a'] == 0) {
                 return false;
             }
-            map.put(c, map.get(c) - 1);
+            alphabets_counter[c - 'a']--;
         }
-
         return true;
     }
 }
