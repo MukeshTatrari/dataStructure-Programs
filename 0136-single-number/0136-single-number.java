500e5785544bcb225ac00ad5e00ecfbe7cebda1b
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-12-01 15:38:55 +0530

    Time: 64 ms (8.62%), Space: 55.5 MB (5.27%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,16 @@
 class Solution {
 
     public int singleNumber(int[] nums) {
-        Map<Integer, Long> occurrences = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
+        var occurrences = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
 
-        AtomicInteger data = new AtomicInteger();
+        final int[] data = new int[1];
         occurrences.forEach(
             (k, v) -> {
                 if (v == 1) {
-                    data.set(k);
+                    data[0] = k;
                 }
             }
         );
-
-        return data.get();
+        return data[0];
     }
 }
