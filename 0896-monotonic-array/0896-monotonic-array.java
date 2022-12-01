cb816aa9d2de00b34430dfa695591aa046d4a673
MukeshTatrari <mukesh.tatrari@gmail.com>
2022-12-01 16:10:27 +0530

    Time: 7 ms (15.04%), Space: 92.3 MB (76.12%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -3,16 +3,14 @@ class Solution {
     public boolean isMonotonic(int[] nums) {
         boolean increase = false;
         boolean decrease = false;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < nums[i - 1]) {
-                decrease = true;
-            }
-            if (nums[i] > nums[i - 1]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
                 increase = true;
             }
+            if (nums[i] > nums[i + 1]) {
+                decrease = true;
+            }
         }
-
         return increase && decrease ? false : true;
     }
 }
